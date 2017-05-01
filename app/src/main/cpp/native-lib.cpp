9fcab63bd5d412106160e1e8c80efd0777c9ce10
kdj <kdj406@gmail.com>
2017-05-01 19:16:55 +0900

    openCV Excercise

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,21 @@
-#include <jni.h>
-#include <string>
+#include<jni.h>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_opencv_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+using namespace cv;
+
+extern "C"{
+JNIEXPORT void JNICALL
+Java_com_example_opencv_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance,
+                                                      jlong matAddrInput, jlong matAddrResult) {
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+
+    cvtColor(matInput, matResult, CV_RGBA2GRAY);
+
 }
+
+}
+
+
--- openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Android\\OpenCV\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\GitHub\\OpenCV\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
